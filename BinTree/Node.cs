b580a8954d0f04fc99422ab504d1daca94420b73
abbotttaa <31903258+abbotttaa@users.noreply.github.com>
2018-02-01 15:06:14 -0600

    Update Node.cs

--- BinTree/Node.cs
+++ BinTree/Node.cs
@@ -18,57 +18,5 @@ namespace BinTree
             rightLeaf = null;
             leftLeaf = null;
         }
-
-        public void insertData(ref Node node, int data)
-        {
-            if (node == null)
-            {
-                node = new Node(data);
-
-            }
-            else if (node.item < data)
-            {
-                insertData(ref node.rightLeaf, data);
-            }
-
-            else if (node.item > data)
-            {
-                insertData(ref node.leftLeaf, data);
-            }
-        }
-
-        public bool search(Node node, int i)
-        {
-            if (node == null)
-                return false;
-
-            if (node.item == i)
-            {
-                return true;
-            }
-            else if (node.item < i)
-            {
-                return search(node.rightLeaf, i);
-            }
-            else if (node.item > i)
-            {
-                return search(node.leftLeaf, i);
-            }
-
-            return false;
-        }
-
-        public void display(Node n)
-        {
-            if (n == null)
-            {
-                return;
-            }
-
-            display(n.leftLeaf);
-            Console.Write(" " + n.item);
-            display(n.rightLeaf);
-        }
-
     }
 }
